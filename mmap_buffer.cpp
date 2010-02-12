1a09ae1fa3a912f1277d9c34276f67cf6372f6b7
Andrew Armenia <armena@andrewsdesktop.inside.asquaredlabs.com>
2010-02-11 19:36:22 -0500

    some minor changes to mmap_buffer

--- mmap_buffer.cpp
+++ mmap_buffer.cpp
@@ -12,81 +12,105 @@
 #define MAGIC 0xdecafbad
 
 MmapBuffer::MmapBuffer(const char *control_file, const char *data_file, unsigned int record_size, bool reset) {
-	struct stat statbuf;
+    struct stat statbuf;
 
-	// assume the files are already there...
-	control_fd = open(control_file, O_RDWR);
-	if (control_fd < 0) {
-		throw std::runtime_error("Failed to open the control file");
-	}		
+    control_fd = -1;
+    data_fd = -1;
+    mmapped_ipc = 0;
+    mmapped_shit = 0;
 
-	mmapped_ipc = (struct control_data *)
-		mmap(0, sizeof(struct control_data), PROT_READ | PROT_WRITE, MAP_SHARED, control_fd, 0);
+    // assume the files are already there...
+    control_fd = open(control_file, O_RDWR);
+    if (control_fd < 0) {
+        throw std::runtime_error("Failed to open the control file");
+    }           
 
-	if (mmapped_ipc == 0) {
-		throw std::runtime_error("Failed to mmap shared state");
-	}
+    mmapped_ipc = (struct control_data *)
+        mmap(0, sizeof(struct control_data), PROT_READ | PROT_WRITE, MAP_SHARED, control_fd, 0);
 
-	if (mmapped_ipc->magic != MAGIC || reset) {
-		// we're the first one here. we must be the source...
-		mmapped_ipc->magic = MAGIC;
-		mmapped_ipc->record_size = record_size;
-		mmapped_ipc->current_timecode = -1;
-		mmapped_ipc->current_offset = 0;
-	}
+    if (mmapped_ipc == 0) {
+        throw std::runtime_error("Failed to mmap shared state");
+    }
 
-	// now open the data file
-	data_fd = open(data_file, O_RDWR);
-	if (data_fd < 0) {
-		throw std::runtime_error("Failed to open data file");
-	}
+    if (mmapped_ipc->magic != MAGIC || reset) {
+        // we're the first one here. we must be the source...
+        mmapped_ipc->magic = MAGIC;
+        mmapped_ipc->record_size = record_size;
+        mmapped_ipc->current_timecode = -1;
+        mmapped_ipc->current_offset = 0;
+    }
 
-	if (fstat(data_fd, &statbuf) < 0) {
-		throw std::runtime_error("fstat on data file failed");
-	}
-	
-	mmapped_ipc->max_offset = statbuf.st_size;
-	n_records = mmapped_ipc->max_offset / mmapped_ipc->record_size;
+    // now open the data file
+    data_fd = open(data_file, O_RDWR);
+    if (data_fd < 0) {
+        throw std::runtime_error("Failed to open data file");
+    }
+
+    if (fstat(data_fd, &statbuf) < 0) {
+        throw std::runtime_error("fstat on data file failed");
+    }
+    
+    mmapped_ipc->max_offset = statbuf.st_size;
+    n_records = mmapped_ipc->max_offset / mmapped_ipc->record_size;
 
 
-	mmapped_shit = (char *)
-		mmap(0, mmapped_ipc->max_offset, PROT_READ | PROT_WRITE, MAP_SHARED, data_fd, 0);
+    mmapped_shit = (char *)
+        mmap(0, mmapped_ipc->max_offset, PROT_READ | PROT_WRITE, MAP_SHARED, data_fd, 0);
 
-	strcpy((char *)mmapped_shit, "Hello World!");
+    strcpy((char *)mmapped_shit, "Hello World!");
 
-	if (mmapped_shit == 0) {
-		throw std::runtime_error("Failed to mmap data buffer");			
-	}
+    if (mmapped_shit == 0) {
+        throw std::runtime_error("Failed to mmap data buffer");         
+    }
+}
+
+// Clean up the memory mappings.
+MmapBuffer::~MmapBuffer( ) {
+    if (0 != mmapped_shit) {
+        munmap(mmapped_shit, mmapped_ipc->max_offset);
+    }
+
+    if (0 != mmapped_ipc) {
+        munmap(mmapped_ipc, sizeof(struct control_data));
+    }
+
+    if (-1 != control_fd) {
+        close(control_fd);
+    }
+
+    if (-1 != data_fd) {
+        close(data_fd);
+    }
 }
 
 int MmapBuffer::put(const void *data, int size) {
-	mmapped_ipc->current_timecode++;
-	mmapped_ipc->current_offset += mmapped_ipc->record_size;
-	if (mmapped_ipc->current_offset > mmapped_ipc->max_offset - mmapped_ipc->record_size) {
-		mmapped_ipc->current_offset = 0;
-	}
-	memcpy((void *)(mmapped_shit + mmapped_ipc->current_offset), data, size);
-	return mmapped_ipc->current_timecode;
+    mmapped_ipc->current_timecode++;
+    mmapped_ipc->current_offset += mmapped_ipc->record_size;
+    if (mmapped_ipc->current_offset > mmapped_ipc->max_offset - mmapped_ipc->record_size) {
+        mmapped_ipc->current_offset = 0;
+    }
+    memcpy((void *)(mmapped_shit + mmapped_ipc->current_offset), data, size);
+    return mmapped_ipc->current_timecode;
 }
 
 bool MmapBuffer::get(void *data, int size, int timecode) {
-	if (
-		mmapped_ipc->current_timecode < timecode 
-		|| mmapped_ipc->current_timecode - n_records > timecode 
-		|| mmapped_ipc->current_timecode == -1
-		|| timecode < 0
-	) {
-		return false;
-	}
+    if (
+        mmapped_ipc->current_timecode < timecode 
+        || mmapped_ipc->current_timecode - n_records > timecode 
+        || mmapped_ipc->current_timecode == -1
+        || timecode < 0
+    ) {
+        return false;
+    }
 
-	long long offset = 
-		mmapped_ipc->current_offset 
-		- ((long long)(mmapped_ipc->current_timecode - timecode) 
-	 	    * mmapped_ipc->record_size);
+    long long offset = 
+        mmapped_ipc->current_offset 
+        - ((long long)(mmapped_ipc->current_timecode - timecode) 
+            * mmapped_ipc->record_size);
 
-	if (offset < 0) {
-		offset += n_records * mmapped_ipc->record_size;
-	}	
-	memcpy(data, (void *)(mmapped_shit+offset), size);
-	return true;
+    if (offset < 0) {
+        offset += n_records * mmapped_ipc->record_size;
+    }       
+    memcpy(data, (void *)(mmapped_shit+offset), size);
+    return true;
 }
--- mmap_buffer.h
+++ mmap_buffer.h
@@ -2,26 +2,27 @@
 #define _MMAP_BUFFER_H
 
 class MmapBuffer {
-	public:
-	MmapBuffer(const char *control_file, const char *data_file, unsigned int record_size, bool reset = false);
-	// keyboard suckage, no destructor for now...
-	int put(const void *data, int size);
-	bool get(void *data, int size, int timecode);
-	
-	private:
-	volatile char *mmapped_shit;
-	
-	// mmap this thing into the control file so that we have shared data among all processes
-	volatile struct control_data {
-		int current_timecode;	
-		long long current_offset;
-		int magic;
-		unsigned long long max_offset;
-		int record_size;
-	} *mmapped_ipc;
+    public:
+    MmapBuffer(const char *control_file, const char *data_file, unsigned int record_size, bool reset = false);
+    ~MmapBuffer( ); 
 
-	int control_fd, data_fd;
-	int n_records;
+    int put(const void *data, int size);
+    bool get(void *data, int size, int timecode);
+    
+    private:
+    volatile char *mmapped_shit;
+    
+    // mmap this thing into the control file so that we have shared data among all processes
+    volatile struct control_data {
+        int current_timecode;   
+        long long current_offset;
+        int magic;
+        unsigned long long max_offset;
+        int record_size;
+    } *mmapped_ipc;
+
+    int control_fd, data_fd;
+    int n_records;
 };
 
 #endif
