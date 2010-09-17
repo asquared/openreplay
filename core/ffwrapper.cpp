ec90b93cce429b67c0927cca5f9d47c386556c58
Andrew Armenia <andrew@asquaredlabs.com>
2010-09-16 21:22:41 -0400

    moved some stuff, fixed alignment

--- core/ffwrapper.cpp
+++ core/ffwrapper.cpp
@@ -1,12 +1,17 @@
 #include "ffwrapper.h"
 
-
-FFwrapper::Decoder::Decoder(enum CodecID id) {
+// just initializes FFmpeg when derived classes are instantiated
+FFwrapper::UsesFFMPEG::UsesFFMPEG( ) {
     if (!avcodec_initialized) {
+        fprintf(stderr, "Initializing FFmpeg libraries...\n");
         avcodec_init( );
-        avcodec_register_all( );
+        av_register_all( );
         avcodec_initialized = true;
     }
+
+}
+
+FFwrapper::Decoder::Decoder(enum CodecID id) {
 
     codec = avcodec_find_decoder(id);
     if (!codec) {
@@ -58,7 +63,7 @@ FFwrapper::Scaler::Scaler(int dstW, int dstH, enum PixelFormat dstFormat) {
 
 FFwrapper::Scaler::~Scaler( ) {
     if (out_buf) {
-        delete [] out_buf;
+        av_free(out_buf);
     }
 
     if (ctx) {
@@ -71,7 +76,7 @@ AVPicture *FFwrapper::Scaler::scale(AVPicture *input, const AVCodecContext *src_
 
     if (!out_buf) {
         picture_size = avpicture_get_size(pixfmt, width, height);
-        out_buf = new uint8_t[picture_size];
+        out_buf = (uint8_t *)av_malloc(picture_size);
         if (!out_buf) {
             throw AllocationError("frame buffer");
         }
@@ -93,4 +98,48 @@ AVPicture *FFwrapper::Scaler::scale(AVPicture *input, const AVCodecContext *src_
     return &out;
 }
 
-bool FFwrapper::Decoder::avcodec_initialized = false;
+FFwrapper::AudioResampler::AudioResampler(int in_ch, int in_rate, enum SampleFormat in_fmt, int out_ch, int out_rate) 
+    : buf(NULL), buf_size(0), ctx(NULL) {
+
+    ctx = av_audio_resample_init(
+        out_ch, in_ch, out_rate, in_rate,       /* in, out parameters */
+        SAMPLE_FMT_S16, in_fmt,         /* output, input formats */
+        16, 10, 0, 0.8                          /* filter length, log2 phase count, linear?, cutoff */
+    );
+
+    if (!ctx) {
+        throw AllocationError("audio resampling context");
+    }
+}
+
+FFwrapper::AudioResampler::~AudioResampler( ) {
+    if (ctx) {
+        audio_resample_close(ctx);
+    }
+
+    if (buf) {
+        av_free(buf);
+    }
+}
+
+short *FFwrapper::AudioResampler::resample(void *in, size_t in_len, size_t *out_len) {
+    *out_len = 2 * in_len; /* FFmpeg needs a way to find this out */
+    if (*out_len > buf_size) {
+        fprintf(stderr, "warning: resample buffer being reallocated\n");
+        buf = (short *) av_realloc(buf, *out_len * sizeof(short));
+        if (!buf) {
+            buf_size = 0;
+            throw AllocationError("audio resample buffer");
+        }
+        buf_size = *out_len;
+    }
+
+    *out_len = audio_resample(ctx, buf, (short *) in, in_len);
+    if (*out_len == 0) {
+        return NULL;
+    } else {
+        return buf;
+    }
+}
+
+bool FFwrapper::UsesFFMPEG::avcodec_initialized = false;
--- core/ffwrapper.h
+++ core/ffwrapper.h
@@ -12,34 +12,6 @@ extern "C" {
 #include <stdexcept>
 
 namespace FFwrapper {
-    class Decoder {
-        public:
-            Decoder(enum CodecID id);
-            ~Decoder( );
-            AVPicture *try_decode(uint8_t *buf, int len);
-            const AVCodecContext *get_ctx( ) { return ctx; }
-        protected:
-            AVFrame *frame;
-            AVCodecContext *ctx;
-            AVCodec *codec;
-            static bool avcodec_initialized;
-    };
-
-    class Scaler {
-        public:
-            Scaler(int dstW, int dstH, enum PixelFormat dstFormat);
-            ~Scaler( );
-            AVPicture *scale(AVPicture *input, const AVCodecContext *src_ctx);
-            uint16_t stride( );
-            uint16_t pitch( );
-        protected:
-            SwsContext *ctx;
-            uint8_t *out_buf;
-            int width, height;
-            enum PixelFormat pixfmt;
-            AVPicture out;
-    };
-
     class FFwrapperError : public std::runtime_error {
         public:
             FFwrapperError(const std::string &s) : std::runtime_error(s) { }
@@ -58,6 +30,54 @@ namespace FFwrapper {
     class CodecError : public FFwrapperError {
         public:
             CodecError(const std::string& s) : FFwrapperError(s) { }
+    };
+
+    /* Classes derived from this one auto-initialize libavcodec and friends */
+    class UsesFFMPEG {
+        public:
+            UsesFFMPEG();
+        protected:
+            static bool avcodec_initialized;
+    };
+
+    class Decoder : public UsesFFMPEG {
+        public:
+            Decoder(enum CodecID id);
+            ~Decoder( );
+            AVPicture *try_decode(uint8_t *buf, int len);
+            const AVCodecContext *get_ctx( ) { return ctx; }
+        protected:
+            AVFrame *frame;
+            AVCodecContext *ctx;
+            AVCodec *codec;
+            static bool avcodec_initialized;
+    };
+
+    class Scaler : public UsesFFMPEG {
+        public:
+            Scaler(int dstW, int dstH, enum PixelFormat dstFormat);
+            ~Scaler( );
+            AVPicture *scale(AVPicture *input, const AVCodecContext *src_ctx);
+            uint16_t stride( );
+            uint16_t pitch( );
+        protected:
+            SwsContext *ctx;
+            uint8_t *out_buf;
+            int width, height;
+            enum PixelFormat pixfmt;
+            AVPicture out;
+    };
+
+    class AudioResampler : public UsesFFMPEG {
+        public:
+            AudioResampler(int in_ch, int in_rate, enum SampleFormat in_fmt, int out_ch, int out_rate);
+            ~AudioResampler( );
+            /* in_len and out_len are in numbers of samples */
+            short *resample(void *in, size_t in_len, size_t *out_len);
+        protected:
+            short *buf;
+            size_t buf_size;
+            ReSampleContext *ctx;
     };
 };
 
