16f5421d0c8323f07437cb7247f6f40c9465072b
Andrew Armenia <andrew@asquaredlabs.com>
2010-11-17 20:14:11 -0500

    add more pixel format conversions to Picture class
    also some minor refactoring

--- core/picture.cpp
+++ core/picture.cpp
@@ -4,11 +4,27 @@
 #include <stdexcept>
 #include <string.h>
 #include <assert.h>
+#include <malloc.h> // memalign
 
 #define align_malloc malloc
 #define align_realloc realloc
 
+/* 16 works out nicely on scanline boundaries but we can go higher*/
+#define ALIGN_ON 64
+
 #define FREELIST_MAX 16
+
+Picture::Picture( ) {
+    data = NULL;
+}
+
+void Picture::alloc_data(size_t size) {
+    if (data) {
+        ::free(data);
+    }
+    data = (uint8_t *)memalign(ALIGN_ON, size);
+    alloc_size = size;
+}
 
 Picture *Picture::alloc(uint16_t w, uint16_t h, uint16_t line_pitch,
         enum pixel_format pix_fmt) {
@@ -19,37 +35,16 @@ Picture *Picture::alloc(uint16_t w, uint16_t h, uint16_t line_pitch,
     if (!free_list.empty( )) {
         candidate = free_list.front( );
         free_list.pop_front( );
-
-        candidate->w = w;
-        candidate->h = h;
-        candidate->line_pitch = line_pitch;
-        candidate->pix_fmt = pix_fmt;
-
-        if (candidate->alloc_size >= pic_size) {
-            return candidate;
-        } else {
-            candidate->data = 
-                (uint8_t *)align_realloc(candidate->data, pic_size);
-            if (!candidate->data) {
-                throw std::runtime_error("Picture resizing failed!\n");
-            }
-            candidate->alloc_size = pic_size;
-            return candidate;
-        }
     } else {
         candidate = new Picture;
-        candidate->w = w;
-        candidate->h = h;
-        candidate->line_pitch = line_pitch;
-        candidate->pix_fmt = pix_fmt;
-        candidate->data = 
-            (uint8_t *)align_malloc(pic_size);        
-        if (!candidate->data) {
-            throw std::runtime_error("New picture allocation failed!\n");
-        }
-        candidate->alloc_size = pic_size;
-        return candidate;
     }
+
+    candidate->w = w;
+    candidate->h = h;
+    candidate->line_pitch = line_pitch;
+    candidate->pix_fmt = pix_fmt;
+    candidate->alloc_data(pic_size);
+    return candidate;
 }
 
 Picture *Picture::copy(Picture *src) {
@@ -74,6 +69,9 @@ Picture *Picture::convert_to_format(enum pixel_format pix_fmt) {
         
         case UYVY8:
             return to_uyvy8( );
+
+        case YUV8:
+            return to_yuv8( );
 
         default:
             throw std::runtime_error("Unknown pixel format requested");
@@ -101,10 +99,29 @@ Picture *Picture::to_uyvy8(void) {
         case UYVY8:
             return Picture::copy(this);
 
+        case YUV8:
+            return yuv8_to_uyvy8( );
+
         default:
             throw std::runtime_error("Cannot convert this format to UYVY8");
     }
 }
+
+Picture *Picture::to_yuv8(void) {
+    switch (this->pix_fmt) {
+        case YUV8:
+            return Picture::copy(this);
+
+        case UYVY8:
+            return uyvy8_to_yuv8( );
+
+        default:
+            throw std::runtime_error("Cannot convert this format to UYVY8");
+    }
+}
+
+#define CLAMP(x) ( (x < 256) ? x : 0 )
+#define SCLAMP(x) ( (x > 0) ? CLAMP(x) : 0 )
 
 Picture *Picture::rgb8_to_uyvy8(void) {
     int i, j;
@@ -149,34 +166,104 @@ Picture *Picture::rgb8_to_uyvy8(void) {
     return out;
 }
 
+/* THIS IS BROKEN and results in video artifacts */
+/* (maybe not anymore??) */
 Picture *Picture::uyvy8_to_rgb8(void) {
     int i, j;
-    uint16_t r, g, b;
+    int16_t r, g, b;
     uint8_t u, y1, v, y2;
     Picture *out = Picture::alloc(this->w, this->h, 3*this->w, RGB8);
     uint8_t *in_ptr, *out_ptr;
 
     for (i = 0; i < this->h; i++) {
-        u = *in_ptr++;
-        y1 = *in_ptr++;
-        v = *in_ptr++;
-        y2 = *in_ptr++;
+        in_ptr = this->scanline(i);
+        out_ptr = out->scanline(i);
 
-        r = (298 * y1 + 409 * v) / 256 - 223;
-        g = (298 * y1 - 100 * u - 208 * v) / 256 + 135;
-        b = (298 * y1 + 516 * v) / 256 - 277;
+        for (j = 0; j < this->w; j += 2) {
+            u = *in_ptr++;
+            y1 = *in_ptr++;
+            v = *in_ptr++;
+            y2 = *in_ptr++;
 
-        *out_ptr++ = r;
-        *out_ptr++ = g;
-        *out_ptr++ = b;
+            r = (298 * y1 + 409 * v) / 256 - 223;
+            g = (298 * y1 - 100 * u - 208 * v) / 256 + 135;
+            b = (298 * y1 + 516 * u) / 256 - 277;
 
-        r = (298 * y2 + 409 * v) / 256 - 223;
-        g = (298 * y2 - 100 * u - 208 * v) / 256 + 135;
-        b = (298 * y2 + 516 * v) / 256 - 277;
+            *out_ptr++ = SCLAMP(r);
+            *out_ptr++ = SCLAMP(g);
+            *out_ptr++ = SCLAMP(b);
 
-        *out_ptr++ = r;
-        *out_ptr++ = g;
-        *out_ptr++ = b;
+            r = (298 * y2 + 409 * v) / 256 - 223;
+            g = (298 * y2 - 100 * u - 208 * v) / 256 + 135;
+            b = (298 * y2 + 516 * u) / 256 - 277;
+
+            *out_ptr++ = SCLAMP(r);
+            *out_ptr++ = SCLAMP(g);
+            *out_ptr++ = SCLAMP(b);
+        }
+    }
+
+    return out;
+}
+
+Picture *Picture::uyvy8_to_yuv8(void) {
+    int i, j;
+    int16_t r, g, b;
+    uint8_t u, y1, v, y2;
+    Picture *out = Picture::alloc(this->w, this->h, 3*this->w, YUV8);
+    uint8_t *in_ptr, *out_ptr;
+
+    for (i = 0; i < this->h; i++) {
+        in_ptr = this->scanline(i);
+        out_ptr = out->scanline(i);
+
+        for (j = 0; j < this->w; j++) {
+            u = *in_ptr++;
+            y1 = *in_ptr++;
+            v = *in_ptr++;
+            y2 = *in_ptr++;
+
+            *out_ptr++ = y1;
+            *out_ptr++ = u;
+            *out_ptr++ = v;
+
+            *out_ptr++ = y2;
+            *out_ptr++ = u;
+            *out_ptr++ = v;
+        }
+    }
+
+    return out;
+}
+
+Picture *Picture::yuv8_to_uyvy8(void) {
+    int i, j;
+    uint16_t u, v;
+    uint8_t y1, y2;
+
+    Picture *out = Picture::alloc(this->w, this->h, 2*this->w, UYVY8);
+    uint8_t *in_ptr, *out_ptr;
+
+    for (i = 0; i < this->h; i++) {
+        in_ptr = this->scanline(i);
+        out_ptr = out->scanline(i);
+
+        for (j = 0; j < this->w; j++) {
+            y1 = *in_ptr++;
+            u = *in_ptr++;
+            v = *in_ptr++;
+            y2 = *in_ptr++;
+            u += *in_ptr++;
+            v += *in_ptr++;
+
+            u /= 2;
+            v /= 2;
+    
+            *out_ptr++ = u;
+            *out_ptr++ = y1;
+            *out_ptr++ = v;
+            *out_ptr++ = y2;
+        }
     }
 
     return out;
--- core/picture.h
+++ core/picture.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 
 enum pixel_format {
-    RGB8, UYVY8
+    RGB8, UYVY8, YUV8
 };
 
 class Picture {
@@ -27,13 +27,19 @@ class Picture {
         
         Picture *convert_to_format(enum pixel_format pix_fmt);
     protected:
+        Picture( );
         Picture *to_rgb8(void);
         Picture *to_uyvy8(void);
+        Picture *to_yuv8(void);
 
         Picture *rgb8_to_uyvy8(void);
         Picture *uyvy8_to_rgb8(void);
+        Picture *yuv8_to_uyvy8(void);
+        Picture *uyvy8_to_yuv8(void);
 
         static std::list<Picture *> free_list;
+
+        void alloc_data(size_t size);
 };
 
 #endif
